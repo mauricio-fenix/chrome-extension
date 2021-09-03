cf622a43156d8d9222332c35f58f9985691520ca
mauricio-fenix <mauricio.bispo@ufvjm.edu.br>
2021-09-03 14:38:41 -0300

    Project init

---
+++ content/constants.js
@@ -0,0 +1,16 @@
+export const DEFINITIONS = {
+  VERSION: {
+    key: version,
+    value: '1.0'
+  },
+
+  STYLE_PATH: {
+    key: style,
+    value: '../src/styles'
+  },
+  SCRIPT_PATH: {
+    key: script,
+    value: '../src/scripts'
+  }
+
+}
