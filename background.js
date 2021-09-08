e21071eef03703251163958c097b26f9d6139c4f
mauricio-fenix <mauricio.bispo@ufvjm.edu.br>
2021-09-08 11:22:46 -0300

    Inserção de CSS funcionando

--- background.js
+++ background.js
@@ -2,6 +2,9 @@ let extensionStatus = 'ON';
 const defaults = {
   height: {
     value: 400
+  },
+  pathCss: {
+    value: "/src/styles/style.css"
   }
 }
 
@@ -13,15 +16,16 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   console.log("onUpdated disparado ...")
   if(tab.url && tab.url.match('[\S]*\.pinterest\.com').length !== 0){
-    const height = defaults.height;
+    const height = defaults.height.value;
     console.log("estou no pinterest ...");
 
     if(changeInfo.status == "loading"){
 
       chrome.scripting.insertCSS({
-        target: { tabId: tabs.id },
-        files: ["inject.css"]
-      }); //editing this
+        target: { tabId: tab.id },
+        files: ["/src/styles/style.css"]
+      }
+      );
 
     } else if(changeInfo.status == "complete"){
         //chrome.tabs.reload(tab.id);
